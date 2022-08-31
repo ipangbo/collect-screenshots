82adaadfbf40c838e391e4f4a723452a2531f5b3
ipangbo <122402331@qq.com>
2022-08-31 21:54:16 +0800

    add gitignore

--- collect_screenshots_bash.sh
+++ collect_screenshots_bash.sh
@@ -9,7 +9,9 @@ for line in $(cat ${namelist})
 do
     echo $line
     read -p "文件地址：" filename
-    echo "原文件删除，保存至终端当前文件夹中target/$line.$extname"
-    mv "$filename" "./target/$line.$extname"
+    echo "原文件未删除，保存至终端当前文件夹中target/$line.$extname"
+    cp "$filename" "./target/line.$extname"
+    # echo "原文件删除，保存至终端当前文件夹中target/$line.$extname"
+    # mv "$filename" "./target/line.$extname"
 done
 echo "程序已退出"
